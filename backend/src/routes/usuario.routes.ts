62a22ea6ff165579ef64a501742e1c79bcdc0bd1
yamiltundis <yamiltundis6@gmail.com>
2026-01-14 21:45:04 -0300

    termino con lo pendiente del anterior commit (usar useFetch en MembreciasPage) y pongo seguridad en algunos endpoints y confirmo que funciona

--- backend/src/routes/usuario.routes.ts
+++ backend/src/routes/usuario.routes.ts
@@ -2,17 +2,18 @@ import { Router } from 'express'
 import * as usuarioController from '../controllers/usuario.controller'
 import { createUserSchema, updateUserSchema } from '../validations/usuario.validations'
 import { validate } from '../middlewares/validation.middleware'
+import { authenticate, authorize } from '../middlewares/auth.middleware'
 
-const router = Router()
+const router = Router();
 
-router.get('/', usuarioController.getAllUsuarios)
+router.get('/', authenticate, authorize('superadmin','admin'), usuarioController.getAllUsuarios)
 
-router.get('/:id', usuarioController.getUsuarioById)
+router.get('/:id', authenticate, authorize('superadmin', 'admin', 'cliente'), usuarioController.getUsuarioById)
 
-router.post('/', validate(createUserSchema), usuarioController.createUsuario)
+router.post('/', authenticate, authorize('superadmin', 'admin'), validate(createUserSchema), usuarioController.createUsuario)
 
-router.put('/:id', validate(updateUserSchema), usuarioController.updateUsuario)
+router.put('/:id', authenticate, authorize('superadmin', 'admin', 'cliente'), validate(updateUserSchema), usuarioController.updateUsuario)
 
-router.delete('/:id', usuarioController.deleteUsuario)
+router.delete('/:id', authenticate, authorize('superadmin'), usuarioController.deleteUsuario)
 
 export const usuarioRoutes = router
--- frontend/src/pages/MembreciasPage.jsx
+++ frontend/src/pages/MembreciasPage.jsx
@@ -1,44 +1,37 @@
 import { useEffect, useState } from "react"
 import '../estilos/membreciasPage.css'
+import { useFetch } from "../hooks/useFetch";
 
 export function MembreciasPage () {
 
   const [membrecias, setMembrecias] = useState([])
 
-   useEffect(() => {
-     const fetchMembrecias = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/tiposMembrecia')
-        if (!response.ok) {
-          throw new Error('Error al traer los tipos de membrecia')
-        }
-        const data = await response.json()
+  const url = 'http://localhost:3000/tiposMembrecia';
+  const { data, loading, error } = useFetch(url, {}, { requireAuth: true });
 
+  const memb = data?.tiposmembrecias || [];
+
+    useEffect(() => {
+    if (!data?.tiposmembrecias) return
+
+      const fetchMembreciasConPrecio = async () => {
         const membreciasConPrecio = await Promise.all(
-            data.tiposmembrecias.map(async (t) => {
-              try {
-                const precioResponse = await fetch(`http://localhost:3000/listasprecios/ultimo/${t.id}`)
-                if (!precioResponse.ok) {
-                  throw new Error('Error al traer precio')
-                }
-                const precioData = await precioResponse.json()
-                return { ...t, precio: precioData.monto } // combinamos membrecia + precio
-              } catch (error) {
-                console.error(`Error al traer precio de membrecia ${t.id}:`, error)
-                return { ...t, precio: 'Desconocido' } // fallback
-              }
-            })
+          data.tiposmembrecias.map(async (t) => {
+            try {
+              const precioResponse = await fetch(`http://localhost:3000/listasprecios/ultimo/${t.id}`)
+              if (!precioResponse.ok) throw new Error('Error al traer el último precio')
+              const precioData = await precioResponse.json()
+              return { ...t, precio: precioData.monto }
+            } catch (error) {
+              return { ...t, precio: 'Desconocido' }
+            }
+          })
         )
-
         setMembrecias(membreciasConPrecio)
-      } catch (error) {
-        console.error(error)
       }
-     }
-
-     fetchMembrecias()
-   }, [])
-
+  
+      fetchMembreciasConPrecio()
+    }, [data])
 
     return (
         <div className="membreciaspage">
