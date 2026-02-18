e6201f8a185d1e3d64b435194cc7e8c418fc698f
yamiltundis <yamiltundis6@gmail.com>
2026-02-18 03:53:48 -0300

    actualizo prisma a su ultima version, hago un docker-compose para levantar la bd, rabbitMQ y redis a la vez

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "mysql://usuario:usuario123@127.0.0.1:3307/gimnasio",
   },
 });
