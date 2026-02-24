b032e98e8b3dd5973c83312a4f2d10780a090f35
yamiltundis <yamiltundis6@gmail.com>
2026-02-23 21:02:27 -0300

    hago que el back no se rompa si no se conecta con redis

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,11 +1,30 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-    url: 'redis://localhost:6379'
+    url: 'redis://localhost:6379',
+    socket: {
+        // Esta función controla cuánto esperar para reintentar
+        reconnectStrategy: (retries) => {
+            if (retries > 2) {
+                console.log("Redis: Máximo de reintentos alcanzado. Se desactiva la reconexión.");
+                return false;
+            }
+            return 5000;
+        }
+    }
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => {
+    console.log('⚠️ Redis offline (opcional)');
+});
 
-redisClient.connect();
+const connectRedis = async () => {
+    try {
+        await redisClient.connect();
+    } catch (err) {
+    }
+};
+
+connectRedis();
 
 export default redisClient;
